27e8aa72ee53a3f497c084abebff4ef0e5d1e856
RafsanRifat <rafsan.rifat255@gmail.com>
2022-09-14 12:57:12 +0600

    token added

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken import views
 from .views import CurrencyListAPIView, CategoryViewSet, TransactionViewSet
 from rest_framework import routers
 
@@ -10,5 +11,6 @@ router.register(r'transactions', TransactionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', views.obtain_auth_token),
     path('currencies/', CurrencyListAPIView.as_view(), name="currencies")
 ]
